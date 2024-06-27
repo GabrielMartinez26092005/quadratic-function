886ea10e6c9f026c1d6497cebe0c21749ed56758
Gabriel Martinez <gaabi26092005@gmail.com>
2024-06-27 15:57:40 -0300

    Correccion de un error matematico y comentarios añadidos

--- ClaseFuncionCuadratica.py
+++ ClaseFuncionCuadratica.py
@@ -1,28 +1,27 @@
 import math
 
+#  Definimos la clase:
 class FuncionCuadratica():
     __coeficientes = []
     __discriminante = 0
     __valores_calculados = []
-
-
+    
+#  Definimos los métodos:
     def ingreso_dato(self):
         self.__coeficientes.append(0)
         while self.__coeficientes[0] == 0:
-            self.__coeficientes[0] = int(input('Ingrese el valor del coeficiente a '))
-        self.__coeficientes.append(int(input('Ingrese el valor del coeficiente b ')))
-        self.__coeficientes.append(int(input('Ingrese el valor del coeficiente c ')))            
-        return
+            self.__coeficientes[0] = int(input('Ingrese el valor del coeficiente a: '))
+            self.__coeficientes.append(int(input('Ingrese el valor del coeficiente b: ')))
+            self.__coeficientes.append(int(input('Ingrese el valor del coeficiente c: ')))
+        return    
     
-
     def calcular_discriminante(self):
         self.__discriminante = self.__coeficientes[1]**2 - 4 * self.__coeficientes[0] * self.__coeficientes[2]
         return
     
-
     def evaluar_discriminante(self):
         if self.__discriminante == 0:
-            self.__valores_calculados.append(self.__coeficientes[1] / 2 * self.__coeficientes[0])
+            self.__valores_calculados.append(-self.__coeficientes[1] / (2 * self.__coeficientes[0]))
         elif self.__discriminante > 0:
             self.__valores_calculados.append((-self.__coeficientes[1] + math.sqrt(self.__discriminante)) / (2 * self.__coeficientes[0]))
             self.__valores_calculados.append((-self.__coeficientes[1] - math.sqrt(self.__discriminante)) / (2 * self.__coeficientes[0]))
@@ -30,23 +29,22 @@ class FuncionCuadratica():
             pass
         return
     
-
     def mostrar_resultado(self):
         if len(self.__valores_calculados) == 0:
             print('La ecuación no tiene solución real')
         elif len(self.__valores_calculados) == 1:
-            print(f'Valor 1: {self.__valores_calculados[0]}')
+            print(f'Valor de la raíz 1: {self.__valores_calculados[0]}')
         else: 
-            print(f'Valor 1: {self.__valores_calculados[0]}\nValor 2: {self.__valores_calculados[1]}')
+            print(f'Valor de la raíz 1: {self.__valores_calculados[0]}\nValor de la raíz 2: {self.__valores_calculados[1]}')
         return
 
-    
+#  Creamos el objeto:
 primer_funcion = FuncionCuadratica()
+#  Llamamos a los métodos
 primer_funcion.ingreso_dato()
 primer_funcion.calcular_discriminante()
 primer_funcion.evaluar_discriminante()
 primer_funcion.mostrar_resultado()
-
 
 
 
